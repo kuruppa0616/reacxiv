ec7826d6eb0766c50aaf2ca26fdce9eafa3eed5c
kuruppa <kuruppa0616@gmail.com>
2019-05-15 23:22:48 +0900

    型定義書き足した

--- types/pixiv-api-client/index.d.ts
+++ types/pixiv-api-client/index.d.ts
@@ -28,7 +28,7 @@ export interface Illust {
 	height: number;
 	sanity_level: number;
 	x_restrict: number;
-	series?: any;
+	series?: Promise<any>;
 	meta_single_page: {
 		original_image_url: string;
 	};
@@ -52,6 +52,29 @@ export interface Tag {
 	translated_name: string;
 }
 
+export interface Credential {
+	access_token: string;
+	expires_in: number;
+	token_type: string;
+	scope: string;
+	refresh_token: string;
+	user: {
+		profile_image_urls: {
+			px_16x16: string;
+			px_50x50: string;
+			px_170x170: string;
+		};
+		id: string;
+		name: string;
+		account: string;
+		mail_address: string;
+		is_premium: boolean;
+		x_restrict: number;
+		is_mail_authorized: boolean;
+	};
+	device_token: string;
+}
+
 declare class PixivApiClient {
 	auth: null | {
 		access_token: string,
@@ -66,25 +89,25 @@ declare class PixivApiClient {
 		'App-Version': '7.2.2',
 		'User-Agent': 'PixivIOSApp/7.2.2 (iOS 12.0.1; iPhone8,2)',
 	};
-	authInfo(): any;
-	bookmarkIllust(id: any, restrict: any, tags: any): any;
-	bookmarkNovel(id: any, restrict: any, tags: any): any;
-	createProvisionalAccount(nickname: any): any;
-	editUserAccount(fields: any): any;
-	followUser(id: any, restrict: any): any;
-	illustAddComment(id: any, comment: any, parentCommentId: any): any;
-	illustBookmarkDetail(id: any, options: any): any;
-	illustCommentReplies(id: any): any;
-	illustComments(id: any, options: any): any;
-	illustCommentsV2(id: any, options: any): any;
-	illustDetail(id: any, options: any): any;
+	authInfo(): Promise<any>;
+	bookmarkIllust(id: number, restrict: any, tags?: string[]): Promise<any>;
+	bookmarkNovel(id: number, restrict: any, tags?: string[]): Promise<any>;
+	createProvisionalAccount(nickname: string): Promise<any>;
+	editUserAccount(fields: any): Promise<any>;
+	followUser(id: number, restrict: any): Promise<any>;
+	illustAddComment(id: number, comment: string, parentCommentid: number): Promise<any>;
+	illustBookmarkDetail(id: number, options: any): Promise<any>;
+	illustCommentReplies(id: number): Promise<any>;
+	illustComments(id: number, options: any): Promise<any>;
+	illustCommentsV2(id: number, options: any): Promise<any>;
+	illustDetail(id: number, options: any): Promise<any>;
 	illustFollow(options?: any): Promise<{
 		illusts: Illust[]
 		next_url: string
 	}>;
-	illustMyPixiv(): any;
-	illustNew(options: any): any;
-	illustRanking(options: any): any;
+	illustMyPixiv(): Promise<any>;
+	illustNew(options: any): Promise<any>;
+	illustRanking(options: any): Promise<any>;
 	illustRecommended(options?: any): Promise<{
 		contest_exists: boolean
 		illusts: Illust[]
@@ -93,79 +116,58 @@ declare class PixivApiClient {
 		ranking_illusts: Illust[]
 
 	}>;
-	illustRelated(id: any, options: any): any;
-	illustWalkthrough(): any;
-	login(username: string, password: string, rememberPassword?: boolean): Promise<{
-		access_token: string;
-		expires_in: number;
-		token_type: string;
-		scope: string;
-		refresh_token: string;
-		user: {
-			profile_image_urls: {
-				px_16x16: string;
-				px_50x50: string;
-				px_170x170: string;
-			};
-			id: string;
-			name: string;
-			account: string;
-			mail_address: string;
-			is_premium: boolean;
-			x_restrict: number;
-			is_mail_authorized: boolean;
-		};
-		device_token: string;
-	}>
-	logout(): any;
-	mangaNew(options: any): any;
-	mangaRecommended(options: any): any;
-	novelAddComment(id: any, comment: any, parentCommentId: any): any;
-	novelBookmarkDetail(id: any, options: any): any;
-	novelCommentReplies(id: any): any;
-	novelComments(id: any, options: any): any;
-	novelCommentsV2(id: any, options: any): any;
-	novelDetail(id: any): any;
-	novelFollow(options: any): any;
-	novelMyPixiv(): any;
-	novelNew(options: any): any;
-	novelRanking(options: any): any;
-	novelRecommended(options: any): any;
-	novelSeries(id: any): any;
-	novelText(id: any): any;
-	refreshAccessToken(refreshToken: any): any;
-	requestUrl(url: any, options: any): any;
-	searchAutoComplete(word: any): any;
+	illustRelated(id: number, options: any): Promise<any>;
+	illustWalkthrough(): Promise<any>;
+	login(username: string, password: string, rememberPassword?: boolean): Promise<Credential>
+	logout(): Promise<void>;
+	mangaNew(options: any): Promise<any>;
+	mangaRecommended(options: any): Promise<any>;
+	novelAddComment(id: number, comment: string, parentCommentid: number): Promise<any>;
+	novelBookmarkDetail(id: number, options: any): Promise<any>;
+	novelCommentReplies(id: number): Promise<any>;
+	novelComments(id: number, options: any): Promise<any>;
+	novelCommentsV2(id: number, options: any): Promise<any>;
+	novelDetail(id: number): Promise<any>;
+	novelFollow(options: any): Promise<any>;
+	novelMyPixiv(): Promise<any>;
+	novelNew(options: any): Promise<any>;
+	novelRanking(options: any): Promise<any>;
+	novelRecommended(options: any): Promise<any>;
+	novelSeries(id: number): Promise<any>;
+	novelText(id: number): Promise<any>;
+	refreshAccessToken(refreshToken: string): Promise<any>;
+	requestUrl(url: string, options: any): Promise<any>;
+	searchAutoComplete(word: string): Promise<any>;
 	searchIllust(word: string, options?: any): Promise<{
 		illusts: Illust[];
 		next_url: string;
 		search_span_limit: number;
 	}>;
-	searchIllustBookmarkRanges(word: any, options: any): any;
-	searchIllustPopularPreview(word: any, options: any): any;
-	searchNovel(word: any, options: any): any;
-	searchNovelBookmarkRanges(word: any, options: any): any;
-	searchNovelPopularPreview(word: any, options: any): any;
-	searchUser(word: any): any;
-	sendAccountVerificationEmail(): any;
-	setLanguage(lang: any): void;
-	trendingTagsIllust(options: any): any;
-	trendingTagsNovel(options: any): any;
-	ugoiraMetaData(id: any): any;
-	unbookmarkIllust(id: any): any;
-	unbookmarkNovel(id: any): any;
-	unfollowUser(id: any): any;
-	userBookmarkIllustTags(options: any): any;
-	userBookmarkNovelTags(options: any): any;
-	userBookmarksIllust(id: any, options: any): any;
-	userBookmarksNovel(id: any, options: any): any;
-	userDetail(id: any, options: any): any;
-	userFollowDetail(id: any): any;
-	userFollower(id: any, options: any): any;
-	userFollowing(id: any, options: any): any;
-	userIllusts(id: any, options: any): any;
-	userMyPixiv(id: any): any;
-	userNovels(id: any, options: any): any;
-	userRecommended(options: any): any;
-	userState(): any;
+	searchIllustBookmarkRanges(word: string, options: any): Promise<any>;
+	searchIllustPopularPreview(word: string, options: any): Promise<any>;
+	searchNovel(word: string, options: any): Promise<any>;
+	searchNovelBookmarkRanges(word: string, options: any): Promise<any>;
+	searchNovelPopularPreview(word: string, options: any): Promise<any>;
+	searchUser(word: string): Promise<any>;
+	sendAccountVerificationEmail(): Promise<any>;
+	setLanguage(lang: any): Promise<void>;
+	trendingTagsIllust(options: any): Promise<any>;
+	trendingTagsNovel(options: any): Promise<any>;
+	ugoiraMetaData(id: number): Promise<any>;
+	unbookmarkIllust(id: number): Promise<any>;
+	unbookmarkNovel(id: number): Promise<any>;
+	unfollowUser(id: number): Promise<any>;
+	userBookmarkIllustTags(options: any): Promise<any>;
+	userBookmarkNovelTags(options: any): Promise<any>;
+	userBookmarksIllust(id: number, options: any): Promise<any>;
+	userBookmarksNovel(id: number, options: any): Promise<any>;
+	userDetail(id: number, options: any): Promise<any>;
+	userFollowDetail(id: number): Promise<any>;
+	userFollower(id: number, options: any): Promise<any>;
+	userFollowing(id: number, options: any): Promise<any>;
+	userIllusts(id: number, options: any): Promise<any>;
+	userMyPixiv(id: number): Promise<any>;
+	userNovels(id: number, options: any): Promise<any>;
+	userRecommended(options: any): Promise<any>;
+	userState(): Promise<any>;
 }
