119f79459b57333a507736226c141985c9951a6b
kuruppa <kuruppa0616@gmail.com>
2019-05-30 23:18:37 +0900

    updateBookmark

--- src/mobx/stores/IllustsStore.ts
+++ src/mobx/stores/IllustsStore.ts
@@ -1,5 +1,5 @@
-import { observable, action, computed, toJS } from 'mobx'
-import { Illust, PixivApiClient, User } from 'pixiv-api-client';
+import { observable, action, computed, toJS, IObservableObject } from 'mobx'
+import { Illust, PixivApiClient } from 'pixiv-api-client';
 import { normalize, schema, denormalize, } from 'normalizr';
 
 import pixivApi from '@/api/PixivApi';
@@ -17,71 +17,78 @@ const illustsSchema = new schema.Array(illustSchema)
 export class IllustsStore {
 
 	private fetch: FetchIllusts
+	private nextUrl: string = "";
 
 	constructor(fetch: FetchIllusts) {
 		this.fetch = fetch
 	}
 
-	@observable.shallow illusts = observable({});
-	@observable.shallow users = observable({});
-	@observable.shallow keys: number[] = observable([]);
-	@observable nextUrl: string = "";
+	@observable.shallow private illusts = observable({});
+	@observable.shallow private users = observable({});
+	@observable.shallow private keys: number[] = observable([]);
 
-	@computed get toJSIllusts() {
-		return toJS(this.illusts)
+	@computed private get toJSIllusts(): any {
+		return toJS(this.illusts);
 	}
 
-	@computed get toJSUsers() {
-		return toJS(this.users)
+	@computed private get toJSUsers(): {} {
+		return toJS(this.users);
 	}
 
-	@computed get toJSKeys() {
-		return toJS(this.keys)
+	@computed private get toJSKeys(): number[] {
+		return toJS(this.keys);
 	}
 
-	@computed get data(): Illust[] {
+	@computed public get data(): Illust[] {
 		const entities = {
 			"illusts": this.toJSIllusts,
 			"users": this.toJSUsers
 		}
-		const denormalized = denormalize(this.toJSKeys, illustsSchema, entities);
-		return denormalized
+
+		return denormalize(this.toJSKeys, illustsSchema, entities);
 	}
 
-	@action setIllusts = (illusts: Illust[]) => {
+	@action private setIllusts = (illusts: Illust[]) => {
 		const normalized = normalize(illusts, illustsSchema);
-		this.illusts = { ...this.illusts, ...normalized.entities.illusts }
-		this.users = { ...this.users, ...normalized.entities.users }
-		this.keys = [...this.keys, ...normalized.result]
+		this.illusts = { ...this.illusts, ...normalized.entities.illusts };
+		this.users = { ...this.users, ...normalized.entities.users };
+		this.keys = [...this.keys, ...normalized.result];
 	}
 
-
-	@action setNextUrl = (url: string) => {
+	@action private setNextUrl = (url: string) => {
 		this.nextUrl = url;
 	}
 
-	@action clearIllusts = () => {
+	@action private clearIllusts = () => {
 		this.illusts = observable({});
 		this.users = observable({});
 		this.keys = observable([]);
 		this.nextUrl = "";
 	}
 
-	fetchIllusts = async () => {
+	@action updateBookmark = (id: number, isBookmarked: boolean) => {
+		console.log("called");
+
+		const illusts: Illust = this.toJSIllusts[id];
+		illusts.is_bookmarked = isBookmarked;
+		this.toJSIllusts[id] = illusts
+	}
+
+	public fetchIllusts = async () => {
 		this.fetch().then(({ illusts, next_url }) => {
-			this.setIllusts(illusts)
-			this.setNextUrl(next_url)
+			this.setIllusts(illusts);
+			this.setNextUrl(next_url);
 		})
 	}
 
-	loadMoreIllusts = async () => {
+	public loadMoreIllusts = async () => {
 		pixivApi.requestUrl(this.nextUrl).then(({ illusts, next_url }) => {
-			this.setIllusts(illusts)
-			this.setNextUrl(next_url)
+			this.setIllusts(illusts);
+			this.setNextUrl(next_url);
 		})
 	}
 
-	reloadIllusts = async () => {
+	public reloadIllusts = async () => {
 		this.clearIllusts();
 		this.fetchIllusts();
 	}
