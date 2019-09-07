52b4ecd743113b8921d9b2ce58c0cfabca91237a
kuruppa <kuruppa0616@gmail.com>
2019-09-07 22:27:30 +0900

    floatingボタン修正

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ import App from './src/App';
 import { name as appName } from './app.json';
 import { YellowBox } from 'react-native';
 
-//TODO:react-native-reanimatedが対応されるまでの応急処置
 YellowBox.ignoreWarnings([
-	'Warning: componentWillMount has been renamed, and is not recommended for use.'
+	//TODO:react-native-reanimatedが対応されるまでの応急処置
+	'Warning: componentWillMount has been renamed, and is not recommended for use.',
+	// illustDetailが原因。今のとこどうしようもない
+	'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead.'
+
 ]);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/components/Button/FloatingBookmarkButton.tsx
+++ src/components/Button/FloatingBookmarkButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { Illust } from 'pixiv-api-client';
 import BookmarkButton from './BookmarkButton';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 interface Props {
 	illust: Illust;
@@ -10,8 +11,11 @@ interface Props {
 const floatingBookmarkButton = (props: Props) => {
 	const { illust, bookmarkFunc } = props;
 	return (
-		<Container style={{ borderRadius: 50 }}>
-			<BookmarkButton illust={illust} size={20} bookmarkFunc={bookmarkFunc} />
+		<Container>
+			<Icon name="circle" size={60} color="#40aaef" />
+			<ButtonWrapper>
+				<BookmarkButton illust={illust} size={25} bookmarkFunc={bookmarkFunc} />
+			</ButtonWrapper>
 		</Container>
 	);
 };
@@ -19,10 +23,15 @@ const floatingBookmarkButton = (props: Props) => {
 const Container = styled.View`
 	width: 60px;
 	height: 60px;
-	background-color: #40aaef;
+	position: relative;
+	height: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+`;
+
+const ButtonWrapper = styled.View`
+	position: absolute;
 `;
 
 export default floatingBookmarkButton;
