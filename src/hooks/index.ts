1b4b5822b5c41226a7e27daf66181c0006a2e43f
kuruppa <kuruppa0616@gmail.com>
2019-09-09 22:03:53 +0900

    delete unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export { default as useBookmark } from './useBookmark';
 export { default as useFollow } from './useFollow';
-export { default as useIllust } from './useIllust';
 export { default as useIllustKeyss } from './useIllustKeys';
 export { default as useUserDetail } from './useUserDetail';
 export { default as useIllustDetail } from './useIllustDetail';
