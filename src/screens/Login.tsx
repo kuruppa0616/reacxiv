2af020bd8959e4004fd18a56ccb4ae3dd97b1e95
kuruppa <kuruppa0616@gmail.com>
2019-09-12 23:13:03 +0900

    ログインできるように

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,29 +1,62 @@
 import React, { useState, useEffect } from 'react';
 
+import { Container, Text, Form, Item, Input, Label, Button, View } from 'native-base';
 import styled from 'styled-components/native';
-import { View, Header, Content, Form, Item, Input, Label } from 'native-base';
+import pixivApi from '@/api/PixivApi';
+import { useNavigation } from 'react-navigation-hooks';
+import { Screens } from '@/constants';
 
-const Login = (props: any) => {
+const Login = () => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
-	useEffect(() => {
-		console.log(username, password);
-	}, [username, password]);
+	const { navigate } = useNavigation();
+
+	const onLogin = () => {
+		pixivApi
+			.login(username, password)
+			.then(() => {
+				navigate(Screens.App);
+			})
+			.catch(() => {
+				navigate(Screens.Auth);
+			});
+	};
 
 	return (
-		<View>
-			<Form>
-				<Item stackedLabel={true}>
+		<StyledContainer>
+			<FormItem>
+				<Item floatingLabel={true}>
 					<Label>Username</Label>
 					<Input value={username} onChangeText={setUsername} />
 				</Item>
-				<Item stackedLabel={true} last={true}>
+			</FormItem>
+			<FormItem>
+				<Item floatingLabel={true}>
 					<Label>Password</Label>
 					<Input value={password} onChangeText={setPassword} secureTextEntry={true} />
 				</Item>
-			</Form>
-		</View>
+			</FormItem>
+			<FormItem>
+				<LoginButton onPress={onLogin}>
+					<Text>Login</Text>
+				</LoginButton>
+			</FormItem>
+		</StyledContainer>
 	);
 };
+
+const StyledContainer = styled(Container)`
+	margin: 0 auto;
+	width: 80%;
+`;
+
+const FormItem = styled(View)`
+	margin-top: 20px;
+`;
+
+const LoginButton = styled(Button)`
+	display: flex;
+	justify-content: center;
+`;
 
 export default Login;
