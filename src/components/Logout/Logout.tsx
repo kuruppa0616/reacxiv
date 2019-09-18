7db8eab5bef3021b7e7511e866bc611a7a336b9d
kuruppa <kuruppa0616@gmail.com>
2019-09-18 23:13:08 +0900

    ログアウトできるように

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
-import { Text, Container, Content, View, Button } from 'native-base';
+import { Text, Container, Content, View, Button, Toast } from 'native-base';
 import styled from 'styled-components/native';
+import pixivApi from '@/api/PixivApi';
+import { Screens } from '@/constants';
+import { useCredential } from '@/hooks';
+import { useNavigation } from 'react-navigation-hooks';
 
 const Logout = () => {
 	const [] = useState(false);
+	const [credential] = useCredential();
+	const { navigate } = useNavigation();
+
+	const onLogout = () => {
+		pixivApi.logout().then(async() => {
+			await credential.reset();
+			navigate(Screens.Login);
+			Toast.show({
+				text: 'logout success!',
+				buttonText: 'OK',
+				type: 'success'
+			});
+		})
+	}
 	return (
 		<Container>
 			<CenteringContent>
 				<Text>Confirm Logout?</Text>
-				<Button danger={true}>
+				<Button onPress={onLogout} danger={true}>
 					<Text>Logout</Text>
 				</Button>
 			</CenteringContent>
